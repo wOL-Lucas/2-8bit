86ac19e7e0285728e4bb772f7a7606104ce614d0
Lucas <lucasefernandes333@gmail.com>
2025-04-15 16:25:07 -0300

    feat: improve file path handling with std::path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 pub mod image_tools;
 use clap::Parser;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -20,18 +21,22 @@ fn main() {
     let args = Args::parse();
 
     let input_image = &args.image;
-    let mut output_image = args.output.clone();
     let pixel_size = args.pixel_size;
     let color_levels = args.color_levels;
-   
+    let output_image = &args.output;
     let img = image::open(input_image).expect("Fail to open image");
     let img_as_8bit = image_tools::to_8bit(&img, pixel_size, color_levels);
-    if output_image.is_empty() {
-        output_image = format!("8bit_{}", input_image);
-    }
 
-    img_as_8bit.save(&output_image)
+    let output_path = if output_image.is_empty() {
+        let input_path = Path::new(input_image);
+        let file_name = input_path.file_name().unwrap_or_default();
+        PathBuf::from("./assets").join(format!("8bit_{}", file_name.to_string_lossy()))
+    } else {
+        PathBuf::from(output_image)
+    };
+
+    img_as_8bit.save(&output_path)
         .expect("Failed to save image");
 
-    println!("Saved 8-bit image to {}", output_image);
+    println!("Saved 8-bit image to {}", output_path.display());
 }
